cdbaa03cef814953c893138d5aef11ea5045c349
sunhwalee <61695175+sunhwa508@users.noreply.github.com>
2020-05-26 16:34:40 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ new fullpage("#fullpage", {
         contimg,
         0.7,
         { x: "160%" },
-        { x: "-10%", ease: Power2.easeInOut },
+        { x: "0%", y: "20%", ease: Power2.easeInOut },
         "-=0.5"
       );
     }
@@ -24,7 +24,7 @@ new fullpage("#fullpage", {
         contimg2,
         1,
         { y: "-200", x: "-200", ease: Power2.easeInOut, opacity: 0 },
-        { x: 0, y: 0, opacity: 1 },
+        { x: 0, y: 150, opacity: 1 },
         "-=0.5"
       );
     }
@@ -33,9 +33,10 @@ new fullpage("#fullpage", {
       const contimg3 = document.querySelector(".img3");
       tl.fromTo(
         contimg3,
-        0.3,
+        0.5,
         { opacity: 0 },
-        { y: "0", ease: Power2.easeInOut, opacity: 1 },
+        { y: "170", ease: Power2.easeInOut, opacity: 1 },
+
         "-=0.5"
       );
     }
@@ -52,20 +53,4 @@ new fullpage("#fullpage", {
   },
 });
 
-// onLeave:(origin,destination,direction) =>{
-//     const section = destination.item;
-//    const title = section.querySelector("h1");
-//    const tl = new TimelineMax({delay:0.5});
-//    tl.fromTo(title, 0.5, {y: '50', opacity: 0}, {y:0, opacity:1});
 
-//     if(destination.index === 1){
-//         const chairs = document.querySelectorAll('.chair');
-//         const description = document.querySelector('.description');
-
-//         tl.fromTo(chairs, 0.5, { x : "100%" }, { x : "-50%" })
-//         .fromTo(description, 0.5, {y: '50', opacity: 0}, {y:0, opacity:1})
-//         .fromTo(chairs[0], 1, {opacity:1}, {opacity:1})
-//         .fromTo(chairs[1], 1, {opacity:0}, {opacity:1})
-//         .fromTo(chairs[2], 1, {opacity:0}, {opacity:1});
-//     }
-// }
